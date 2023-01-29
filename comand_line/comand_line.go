29b4c158d8774075ff07c48c3689ecca6b58e1f1
Halgenthaler <hans.halgenthaler@gmail.com>
2023-01-29 18:50:03 -0300

    My second  Commit

--- comand_line/comand_line.go
+++ comand_line/comand_line.go
@@ -12,9 +12,27 @@ func main() {
 	for i := 1; i < len(os.Args); i++ {
 		s = s + sep + os.Args[i]
 
-		sep = ""
+		sep = " "
 	}
 
 	fmt.Println(s)
+	test_main()
 
+}
+
+// The other way
+
+func test_main() {
+
+	s := ""
+	var i int
+	for j, arg := range os.Args[1:] {
+		i = i + 1
+		fmt.Println(j)
+		s = string(j) + arg
+
+		fmt.Println(s)
+	}
+
+	fmt.Println(s)
 }
